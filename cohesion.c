0a86aff2801fdb4406c07621ba77c6f1d7ebf580
Chris Berglund <13christopherb@gmail.com>
2020-01-17 17:49:34 -0800

    Make cohesion function use fewer if statements

--- cohesion.c
+++ cohesion.c
@@ -22,26 +22,21 @@ bool isCohesive(const int *window, int width, int threshold) {
     double t2 = 0;
     for (int i = 1; i < width - 1; i++) {
         for (int j = 1; j < width - 1; j++) {
-            if (copy[i * width + j] == 0) {
-                if (copy[i * width + j] == copy[(i - 1) * width + j]) r1++;
-                if (copy[i * width + j] == copy[(i + 1) * width + j]) r1++;
-                if (copy[i * width + j] == copy[i * width + (j - 1)]) r1++;
-                if (copy[i * width + j] == copy[i * width + (j + 1)]) r1++;
-                if (copy[i * width + j] == copy[(i - 1) * width + (j - 1)]) r1++;
-                if (copy[i * width + j] == copy[(i + 1) * width + (j - 1)]) r1++;
-                if (copy[i * width + j] == copy[(i - 1) * width + (j + 1)]) r1++;
-                if (copy[i * width + j] == copy[(i + 1) * width + (j + 1)]) r1++;
-                t1 += 8;
-            } else if (copy[i * width + j] == 1) {
-                if (copy[i * width + j] == copy[(i - 1) * width + j]) r2++;
-                if (copy[i * width + j] == copy[(i + 1) * width + j]) r2++;
-                if (copy[i * width + j] == copy[i * width + (j - 1)]) r2++;
-                if (copy[i * width + j] == copy[i * width + (j + 1)]) r2++;
-                if (copy[i * width + j] == copy[(i - 1) * width + (j - 1)]) r2++;
-                if (copy[i * width + j] == copy[(i + 1) * width + (j - 1)]) r2++;
-                if (copy[i * width + j] == copy[(i - 1) * width + (j + 1)]) r2++;
-                if (copy[i * width + j] == copy[(i + 1) * width + (j + 1)]) r2++;
-                t2 += 8;
+            for (int k = 0; k < 3; k ++) {
+                if (copy[i * width + j] == 0 ) {
+                    t1 += 8;
+                } else if (copy[i * width + j] == 1) {
+                    t2 += 8;
+                }
+                for (int m = 0; m < 3; m ++) {
+                    if (k == 1 && m == 1) {
+                        continue;
+                    }
+                    if (copy[i * width + j] == 0 && copy[(i + k - 1) * width + (j + m - 1)] == 0)
+                        r1++;
+                    else if (copy[i * width + j] == 1 && copy[(i + k - 1) * width + (j + m - 1)] == 1)
+                        r2++;
+                }
             }
         }
     }
