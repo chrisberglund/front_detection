c11738c5f23dafbecf0ded29ec7b22e13677d11e
Chris Berglund <13christopherb@gmail.com>
2020-07-30 02:16:31 +0000

    Debug

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-gcc -std=gnu99 -c -fPIC -pthread -o filter.o filter.c
-gcc -std=gnu99 -c -fPIC -pthread -o cayula.o cayula.c
-gcc -std=gnu99 -c -fPIC -pthread -o helpers.o helpers.c
-gcc -std=gnu99 -c -fPIC -pthread -o cohesion.o cohesion.c
-gcc -std=gnu99 -c -fPIC -pthread -o contour.o contour.c
-gcc -std=gnu99 -c -fPIC -pthread -o histogram.o histogram.c
+gcc -std=gnu99 -c -g -fPIC -pthread -o filter.o filter.c
+gcc -std=gnu99 -c -g -fPIC -pthread -o cayula.o cayula.c
+gcc -std=gnu99 -c -g -fPIC -pthread -o helpers.o helpers.c
+gcc -std=gnu99 -c -g -fPIC -pthread -o cohesion.o cohesion.c
+gcc -std=gnu99 -c -g -fPIC -pthread -o contour.o contour.c
+gcc -std=gnu99 -c -g -fPIC -pthread -o histogram.o histogram.c
 
-gcc -shared -fPIC -pthread -o ../sied.so filter.o cayula.o helpers.o cohesion.o contour.o histogram.o
+gcc -shared -fPIC -pthread -g -o ../sied.so filter.o cayula.o helpers.o cohesion.o contour.o histogram.o
 
